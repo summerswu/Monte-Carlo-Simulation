2e604533f16699e3fa001437758be1501842026a
summers wu <wlysummers@gmail.com>
2019-05-29 19:16:32 -0400

    dependent on evenlet to actually work, worker server not functioning on windows

--- backend/restapi.py
+++ backend/restapi.py
@@ -1,29 +1,40 @@
 from flask import Flask, request, jsonify
-from algorithm import approx
 from worker import integrate
-
+ 
+# to run:
+# $ python3 restapi.py
+ 
 app = Flask(__name__)
 TASKS = {}
-
-@app.route('/', methods=["GET"])
+ 
+@app.route('/', methods=['GET'])
 def list_tasks():
-	tasks = { task_id: {'ready':task.ready}
-			  for task_id, task in TASKS.item()}
-	return jsonify(tasks)
-
-@app.route('/', methods = ['PUT'])
+    tasks = {task_id: {'ready': task.ready()}
+             for task_id, task in TASKS.items()}
+    return jsonify(tasks)
+ 
+@app.route('/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    response = {'task_id': task_id}
+ 
+    task = TASKS[task_id]
+    if task.ready():
+        response['result'] = task.get()
+    return jsonify(response)
+ 
+@app.route('/', methods=['PUT'])
 def put_task():
-	f = request.json['f']
-	a = request.json['a']
-	b = request.json['b']
-	c = request.json['c']
-	d = request.json['d']
-	size = request.json.get('size',100)
-
-	response = {
-		'result': integrate.delay(f,a,b,c,d,size),
-	}
-	return jsonify(response)
-
+    f = request.json['f']
+    a = request.json['a']
+    b = request.json['b']
+    c = request.json['c']
+    d = request.json['d']
+    size = request.json.get('size', 100)
+ 
+    task_id = len(TASKS)
+    TASKS[task_id] = integrate.delay(f, a, b, c, d, size)
+    response = {'result': task_id}
+    return jsonify(response)
+ 
 if __name__ == '__main__':
-	app.run(debug=True) 
+    app.run(debug=True)
